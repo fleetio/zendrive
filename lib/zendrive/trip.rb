28559b9331b23a0690285712396ae4e636e0c5dd
Jorge Valdivia <jvldivia@rarestep.com>
2016-11-07 09:38:08 -0600

    add speed profile to trip

--- lib/zendrive/trip.rb
+++ lib/zendrive/trip.rb
@@ -4,7 +4,7 @@ module Zendrive
     SINGLE_ENDPOINT = "driver/{driver_id}/trip/{trip_id}"
     RESOURCE_NAME = "trips"
 
-    attr_reader :id, :trip_id, :info, :score, :simple_path, :events
+    attr_reader :id, :trip_id, :info, :score, :simple_path, :events, :speed_profile
 
     def initialize(attributes)
       @id = attributes["trip_id"]
@@ -14,12 +14,13 @@ module Zendrive
       @score = Util::DeepStruct.new(attributes["score"]) if attributes["score"] && attributes["score"].any?
 
       @simple_path = attributes["simple_path"]
+      @speed_profile = attributes["speed_profile"]
       @events = attributes["events"]
     end
 
     def self.find(driver_id, trip_id)
       params = default_params.dup
-      params[:params].merge!({fields: "info,score,simple_path,events"})
+      params[:params].merge!({fields: "info,score,simple_path,events,speed_profile"})
 
       response = RestClient.get(
         url_for(self::SINGLE_ENDPOINT, {driver_id: driver_id, trip_id: trip_id}),
