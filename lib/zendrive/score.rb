7d6d0cb069b7d3b00d665252a0ced0febc1716ff
Jorge Valdivia <jorge+dev@valdivia.me>
2016-12-05 15:32:29 -0600

    convert km to mi and hours to seconds

--- lib/zendrive/score.rb
+++ lib/zendrive/score.rb
@@ -6,6 +6,8 @@ module Zendrive
     attr_reader :info, :score, :score_statistics, :start_date, :end_date
 
     def initialize(attributes)
+      attributes = format_attrs(attributes)
+
       @info = Util::DeepStruct.new(attributes["info"]) if attributes["info"] && attributes["info"].any?
       @score = Util::DeepStruct.new(attributes["score"]) if attributes["score"] && attributes["score"].any?
       @score_statistics = Util::DeepStruct.new(attributes["score_statistics"]) if attributes["score_statistics"] && attributes["score_statistics"].any?
@@ -22,6 +24,27 @@ module Zendrive
       response = RestClient.get(url_for(self::SINGLE_ENDPOINT, {driver_id: driver_id}), req_params)
 
       new(JSON.parse(response.body))
+    end
+
+    private
+
+    def format_attrs(attributes)
+      if attributes["info"] && attributes["info"].any? && attributes["info"]["distance_km"]
+        attributes["info"]["distance_in_km"] = attributes["info"]["distance_km"].to_f
+        attributes["info"]["distance_in_mi"] = attributes["info"]["distance_km"].to_f * 0.621371
+        attributes["info"]["drive_time_in_seconds"] = convert_drive_hours_to_seconds(attributes["info"]["drive_time_hours"])
+      end
+
+      attributes
+    end
+
+    def convert_drive_hours_to_seconds(hours_string)
+      if hours_string
+        hours, minutes = hours_string.split(":").map(&:to_i)
+        (hours * 3600) + (minutes * 60)
+      else
+        0
+      end
     end
   end
 end
