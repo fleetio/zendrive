5f5bcb2299f73cf753316be511a4b8575bc480fb
Jorge Valdivia <jorge+dev@valdivia.me>
2016-12-06 09:38:56 -0600

    add findable to driver so that we can get the recommendation

--- lib/zendrive/driver.rb
+++ lib/zendrive/driver.rb
@@ -15,6 +15,17 @@ module Zendrive
       @recommendation = attributes["recommendation"]
     end
 
+    def self.find(id)
+      params = default_params.dup
+      params[:params].merge!({ids: id})
+
+      response = RestClient.get(url_for(self::INDEX_ENDPOINT, {}), params)
+      drivers = JSON.parse(response.body)
+      if drivers && drivers["drivers"] && drivers["drivers"].length > 0
+        new(drivers["drivers"].first)
+      end
+    end
+
     def trips
       Trip.all(driver_id: id)
     end
